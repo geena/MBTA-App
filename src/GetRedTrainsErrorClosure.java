fbe5b7558c9c5fe1a45b8ad32af67454a5ca814f
Thomas Meng <tmeng999@gmail.com>
2012-10-31 18:55:54 -0400

    added current location package

--- src/GetRedTrainsErrorClosure.java
+++ src/GetRedTrainsErrorClosure.java
@@ -2,7 +2,7 @@ import models.MBTAResponse;
 import requests.AbstractMBTARequest;
 import requests.Closure;
 
-public class GetRedTrainsErrorClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+class GetRedTrainsErrorClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
 {
 	@Override
 	public Class<MBTAResponse> getResponseType()
---
+++ src/currentlocation/CurrentLocation.java
@@ -0,0 +1,210 @@
+package currentlocation;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.MBTAResponse;
+import models.Predictions;
+import models.TripList;
+import models.Trips;
+import requests.AbstractMBTARequest;
+import requests.Closure;
+import services.MBTAService;
+
+public class CurrentLocation
+{
+	private final MBTAService _mbtaService;
+	private List<TripList> _allTrainsList;
+
+	public CurrentLocation() 
+	{
+		_mbtaService = new MBTAService();
+	}
+
+	private void getLocation()
+	{
+		getRedLineTrains();
+		getBlueLineTrains();
+		getOrangeLineTrains();
+	}
+	
+	private void getOrangeLineTrains()
+	{
+		GetOrangeTrainsSuccessClosure successClosure = new GetOrangeTrainsSuccessClosure();
+		GetOrangeTrainsErrorClosure errorClosure = new GetOrangeTrainsErrorClosure();
+		GetOrangeTrainsCancelClosure cancelClosure = new GetOrangeTrainsCancelClosure();
+		
+		_mbtaService.getOrangeLineTrains(successClosure, errorClosure, cancelClosure);
+	}
+	
+	private void getBlueLineTrains()
+	{
+		GetBlueTrainsSuccessClosure successClosure = new GetBlueTrainsSuccessClosure();
+		GetBlueTrainsErrorClosure errorClosure = new GetBlueTrainsErrorClosure();
+		GetBlueTrainsCancelClosure cancelClosure = new GetBlueTrainsCancelClosure();
+		
+		_mbtaService.getBlueLineTrains(successClosure, errorClosure, cancelClosure);
+	}
+	
+	private void getRedLineTrains()
+	{
+		GetRedTrainsSuccessClosure successClosure = new GetRedTrainsSuccessClosure();
+		GetRedTrainsErrorClosure errorClosure = new GetRedTrainsErrorClosure();
+		GetRedTrainsCancelClosure cancelClosure = new GetRedTrainsCancelClosure();
+		
+		_mbtaService.getRedLineTrains(successClosure, errorClosure, cancelClosure);
+	}
+	
+	private class GetRedTrainsCancelClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			return null;
+		}
+	}
+
+	private class GetRedTrainsErrorClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			Exception exception = data.getException();
+			System.out.println("ERROR: " + exception.getMessage());
+
+			return null;
+		}
+	}
+
+	private class GetRedTrainsSuccessClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			TripList tripList = data.getTripList();
+			_allTrainsList.add(tripList);
+			return null;
+		}
+	}
+	
+	private class GetBlueTrainsCancelClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			return null;
+		}
+	}
+
+	private class GetBlueTrainsErrorClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			Exception exception = data.getException();
+			System.out.println("ERROR: " + exception.getMessage());
+
+			return null;
+		}
+	}
+
+	private class GetBlueTrainsSuccessClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			TripList tripList = data.getTripList();
+			_allTrainsList.add(tripList);
+			return null;
+		}
+	}
+	
+	private class GetOrangeTrainsCancelClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			return null;
+		}
+	}
+
+	private class GetOrangeTrainsErrorClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			Exception exception = data.getException();
+			System.out.println("ERROR: " + exception.getMessage());
+
+			return null;
+		}
+	}
+
+	private class GetOrangeTrainsSuccessClosure implements Closure<Void, AbstractMBTARequest, MBTAResponse>
+	{
+		@Override
+		public Class<MBTAResponse> getResponseType()
+		{
+			return null;
+		}
+
+		@Override
+		public Void invoke(AbstractMBTARequest caller, MBTAResponse data)
+		{
+			TripList tripList = data.getTripList();
+			_allTrainsList.add(tripList);
+			
+			//Here is where you want to do some stuff with the _allTrainsList
+			
+			return null;
+		}
+	}
+
+}
