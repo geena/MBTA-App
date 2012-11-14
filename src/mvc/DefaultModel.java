2693fe21d73a426cfe8db9199d1bc11b61c29fc7
geena <raginaranstrom@gmail.com>
2012-11-14 18:41:59 -0500

    did some stuff

--- src/mvc/DefaultModel.java
+++ src/mvc/DefaultModel.java
@@ -7,4 +7,13 @@ public abstract class DefaultModel{
 	
 	protected final Map<Object, Object> _data = new HashMap<Object, Object>();
 	
+	public <T> T getValue(String key, T defaultValue)
+	{
+		T value = (T) _data.get(key);
+		if (value == null)
+		{
+			value = defaultValue;
+		}
+		return value;
+	}
 }
