0dbc706ab90fcce6c3d380f5f33aacf718d49f14
geena <raginaranstrom@gmail.com>
2012-11-14 20:18:24 -0500

    finished mvc framework

--- src/mvc/DefaultView.java
+++ src/mvc/DefaultView.java
@@ -1,7 +1,30 @@
 package mvc;
 
-public abstract class DefaultView
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class DefaultView implements ModelEventListener
 {
 	private DefaultModel _model;
-	protected abstract void processModelChange();
+	private final List<ViewEventListener> _listeners = new ArrayList<ViewEventListener>();
+	
+	protected void addViewEventListener(ViewEventListener listener)
+	{
+		_listeners.add(listener);
+	}
+	
+	protected void removeViewEventListener(ViewEventListener listener)
+	{
+		_listeners.remove(listener);
+	}
+	
+	public void notifyViewEvent(String event)
+	{
+		for (ViewEventListener listener: _listeners)
+		{
+			listener.processViewEvent(event);
+		}
+	}
+	
+	public abstract void modelChanged(String event);
 }
