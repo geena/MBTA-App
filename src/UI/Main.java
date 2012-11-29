0f7695e35437208d503d8b45adb8cbdd9cbdcdc3
Thomas Meng <tmeng999@gmail.com>
2012-11-28 19:22:08 -0500

    added useroptions

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -2,15 +2,10 @@ package UI;
 
 
 import java.awt.Color;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.Timer;
 
 
 
